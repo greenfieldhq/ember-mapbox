ff14bda765460816f073b8192c1c321cc9c027ca
Mike Munroe <mike@mikemunroe.com>
2014-04-18 14:06:15 -0400

    remove default ember starter model

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4,13 +4,7 @@ App.Router.map(function() {
   // put your routes here
 });
 
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
-
-App.config = { 
+App.config = {
   api_key: 'greenfield.i0p4gl0k',
   center: [42.35, -71.051],
   zoom: 15
