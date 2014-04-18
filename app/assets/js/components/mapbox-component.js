60517b90fd520fd577e02bb196997b15135601f2
Ryan Tremaine <ryan.trem@gmail.com>
2014-04-18 14:04:04 -0400

    draw test data on map

--- app/assets/js/components/mapbox-component.js
+++ app/assets/js/components/mapbox-component.js
@@ -1,5 +1,21 @@
 App.MapboxMapComponent = Ember.Component.extend({
+  markers: function() {
+    return App.data.companies;
+  }.property(),
   didInsertElement: function() {
+    this._super();
     App.map = L.mapbox.map('map', App.config.api_key).setView(App.config.center, App.config.zoom);
+
+    this.get('markers').forEach(function(marker) {
+      var marker = L.marker([marker.latitude, marker.longitude], {
+          icon: L.divIcon({
+            className: '',
+            html: 'test',
+            iconSize: [30, 30]
+          })
+      });
+
+      marker.addTo(App.map);
+    }); 
   }
 });
