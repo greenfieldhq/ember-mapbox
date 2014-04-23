df236c98bf16b6fbfbb5fbd8fc8f744e6e596eff
Ryan Tremaine <ryan.trem@gmail.com>
2014-04-23 11:44:04 -0400

    added a scroll style. added additional fields to the sample data.

--- app/components/mapbox-component.js
+++ app/components/mapbox-component.js
@@ -42,5 +42,5 @@ App.MapboxMapComponent = Ember.Component.extend({
 });
 
 App.DefaultPopupView = Ember.View.extend({
-  template: Ember.Handlebars.compile('<ul><li>name: <p>{{name}}</p></li><li>description: {{{description}}}</li></ul>')
+  template: Ember.Handlebars.compile('<ul><li>name: <p>{{name}}</p></li><li>description: <div id="description">{{{description}}}</div></li></ul>')
 });
